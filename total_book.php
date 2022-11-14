7b99fbe24a547649285d1ec01201947a4ce50df5
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-14 22:27:26 +0900

    Update total_book.php

--- total_book.php
+++ total_book.php
@@ -3,3 +3,22 @@
 
 ?>
     <h2> 전체 도서 </h2>
+<?php
+  $query = "SELECT NAME, BID
+           FROM BOOK
+           ORDER BY NAME";
+
+  $stmt = $conn -> prepare($query);
+  $stmt -> execute();
+
+while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
+  $name = $row['NAME'];
+  $bid = $row['BID'];
+?>
+
+  <p> <a href="book_detail.php?bid=<?=$bid?>"> <?= $name?> </a></p>
+
+  <?php
+  }
+  ?>
+post방식으로 a태그 어케 넘김????
