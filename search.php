f6863cfc01ffef7eefdcad08b0a2d0094483ee8e
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-14 23:52:51 +0900

    Update search.php

--- search.php
+++ search.php
@@ -1,5 +1,25 @@
 <?php
  require('menu.php');
-
+$book_name = $_POST['book_name'];
+$book_name = "%".$book_name."%";
 ?>
     <h2>도서 검색</h2>
+<?php
+  $query = "SELECT NAME, BID
+           FROM BOOK
+           WHERE NAME LIKE ?
+           ORDER BY NAME";
+
+  $stmt = $conn -> prepare($query);
+  $stmt -> execute(array($book_name));
+
+while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
+  $name = $row['NAME'];
+  $bid = $row['BID'];
+?>
+
+  <p> <a href="book_detail.php?bid=<?=$bid?>"> <?= $name?> </a></p>
+
+  <?php
+  }
+  ?>
