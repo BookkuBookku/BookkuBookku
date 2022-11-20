bc77af49cb56d64ea438cbe3dfb0b03d7c022e55
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-20 23:07:50 +0900

    Update attendance.php

--- attendance.php
+++ attendance.php
@@ -3,3 +3,22 @@
 
 ?>
     <h2>출석체크</h2>
+<?php
+  $query = "SELECT COUNTS
+         FROM ATTEND
+         WHERE ID = ?";
+
+  $stmt = $conn -> prepare($query);
+  $stmt -> execute(array($id));
+
+  if (!empty($row = $stmt -> fetch(PDO::FETCH_ASSOC))){
+    $counts = $row['COUNTS'];
+    echo "누적 출석 횟수: ".$counts;
+
+  }
+
+?>
+<form method="POST" action="attendance_process.php"><!-- 출석 버튼 -->
+  <input type="hidden" name="id" value="<?= $id ?>"/>
+  <p> <input type="submit" value="출석하기"/> </p>
+</form>
