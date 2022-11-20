3dc66467f36730838300b4089aacbb996e01edcd
efjwbe <choeun123456@daum.net>
2022-11-20 14:20:57 +0900

    constructed menu / my_lib / total_book

--- total_book.php
+++ total_book.php
@@ -2,7 +2,16 @@
  require('menu.php');
 
 ?>
-    <h2> 전체 도서 </h2>
+<nav class="navbar bg-light">
+  <div class="container-fluid" style="padding: 0px 75px 0px 60px;">
+    <a class="navbar-brand" style="font-size:2em;"><b>전체 도서</b></a>
+      <form class="d-flex" role="search" method="POST" action="search.php">
+        <input class="form-control me-2" type="text" name="book_name" placeholder="책 제목" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+  </div>
+</nav>
+
 <?php
   $query = "SELECT NAME, BID
            FROM BOOK
