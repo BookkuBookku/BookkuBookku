53d3f6908fca1f3706e745dca64a3def1755d6bb
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-20 21:50:31 +0900

    Update my_lib.php

--- my_lib.php
+++ my_lib.php
@@ -27,20 +27,84 @@
 
 
 <?php
- if($_GET['id']=='reding'){?>
+$s = $_GET['s'];
+ if($s =='reading'){?>
    <h2>읽던 책</h2>
- <?php
-}elseif ($_GET['id']=='like') {?>
+   <?php
+     $query = "SELECT DISTINCT BOOK.NAME, BOOK.AUTHOR, BOOK.BID
+              FROM MY_LIB, BOOK
+              WHERE BOOK.BID = MY_LIB.BID
+                AND MY_LIB.ID = ? AND MY_LIB.READING = 'T' ";
+
+     $stmt = $conn -> prepare($query);
+     $stmt -> execute(array($id));
+
+   while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
+     if(empty($row)){
+       echo "읽던 책이 없습니다.";
+     }
+     $name = $row['NAME'];
+     $bid = $row['BID'];
+   ?>
+     <p> <a href="book_detail.php?bid=<?=$bid?>"> <?= $name?> </a></p>
+     <?php
+     }
+
+}elseif ($s =='like') {?>
   <h2>찜한 책</h2>
-<?php
+  <?php
+    $query = "SELECT DISTINCT BOOK.NAME, BOOK.AUTHOR, BOOK.BID
+             FROM MY_LIB, BOOK
+             WHERE BOOK.BID = MY_LIB.BID
+               AND MY_LIB.ID = ? AND MY_LIB.BOOK_LIKE = 'T' ";
 
+    $stmt = $conn -> prepare($query);
+    $stmt -> execute(array($id));
 
-}elseif ($_GET['id']=='sentence') {?>
+  while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
+    if(empty($row)){
+      echo "찜한 책이 없습니다.";
+    }
+
+    $name = $row['NAME'];
+    $bid = $row['BID'];
+  ?>
+    <p> <a href="book_detail.php?bid=<?=$bid?>"> <?= $name?> </a></p>
+    <?php
+    }
+
+}elseif ($s =='sentence') {?>
   <h2>내 문구</h2>
-<?php
+  <?php
+    $query = "SELECT BOOK.NAME, BOOK.AUTHOR, BOOK.BID, PHASE.ROUTE
+             FROM PHASE, BOOK
+             WHERE BOOK.BID = PHASE.BID
+               AND PHASE.ID = ?";
 
+    $stmt = $conn -> prepare($query);
+    $stmt -> execute(array($id));
 
+  while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
+    if(empty($row)){
+      echo "작성한 문구가 없습니다.";
+    }
+    $name = $row['NAME'];
+    $bid = $row['BID'];
+    $route = $row['ROUTE'];
+  ?>
+    <p> <a href="phase_detail.php?bid=<?=$bid?>"> <?= $name?> </a></p>
+    <?php
+    //파일 열기
+    $fp = fopen($route, "r") or die("문장을 불러올 수 없습니다.");
+    // 파일 내용 출력
+
+    while( !feof($fp) ) {
+      $member = fgets($fp); // 한 줄씩 $member 변수에 저장하고
+      echo $member."<br>";
+    }
+
+    // 파일 닫기
+    fclose($fp);
+    }
 }
-
-
 ?>
