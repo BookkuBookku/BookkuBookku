75b7f714594a6811d93476db3fd9f4b7c7f264e4
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-20 23:59:50 +0900

    Update book_process.php

--- book_process.php
+++ book_process.php
@@ -13,6 +13,41 @@ try {
 
 $bid = $_POST['bid'];
 $id = $_POST['id'];
+$status = $_POST['status'];
 
+//둘다 없는 경우->새로 만들어야함
+
+
+
+
+
+
+
+
+//둘중 하나 있는 경우 -> uodate로 수정
+
+
+
+
+
+
+
+
+if ($status == 'reading'){
+
+
+
+}elseif ($status == 'like'){
+
+}
+
+$query = "INSERT INTO BOOK_USER (LOGIN_ID, PASSWORD, NAME)
+            VALUES (:login_id, :pwd, :name)";
+$stmt = $dbh -> prepare($query);
+$stmt->bindParam(':login_id',$login_id);
+$stmt->bindParam(':pwd',$pwd);
+$stmt->bindParam(':name',$name);
+
+//header("Refresh: 0; URL=cancle.php");
 
 ?>
