d04c6de2d061e8075b11ee2cc27f0c13dc5a259e
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-22 21:59:14 +0900

    Update write_process.php

--- write_process.php
+++ write_process.php
@@ -17,35 +17,15 @@ try {
   $phase_date = date("Y-m-d");
 
   //입력 받은 내용 업데이트
-  $query1 = "INSERT INTO PHASE (BID, ID, PHASE_DATE)
-                VALUES (:bid, :id, :phase_date)";
+  $query1 = "INSERT INTO PHASE (BID, ID, SENTENCE, PHASE_DATE)
+                VALUES (:bid, :id, :sentence, :phase_date)";
   $stmt = $conn -> prepare($query1);
   $stmt->bindParam(':bid',$bid);
   $stmt->bindParam(':id',$id);
+  $stmt->bindParam(':sentence',$sentence);
   $stmt->bindParam(':phase_date',$phase_date);
   $stmt -> execute();
 
-  //sentence 업데이트 및 저장
-  $query2 = "SELECT MAX(PID) PID FROM PHASE";
-  $stmt = $conn -> prepare($query2);
-  $stmt -> execute();
-
-  while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){
-    $pid = $row['PID'];
-  }
-
-  $route = $bid."_".$pid."\.txt";
-  //문장 파일 생성: 오류 발생
-  $myfile = @fopen($route, "w") or die("Unable to open file!");
-  $txt = $sentence;
-  fwrite($myfile, $txt);
-  fclose($myfile);
-
-  //DB에 경로 추가
-  $query3 = "UPDATE PHASE SET route = :route WHERE PID = :pid";
-  $stmt = $conn -> prepare($query3);
-  $stmt->bindParam(':route',$route);
-  $stmt->bindParam(':pid',$pid);
-  $stmt -> execute()
-
+  echo "<script>alert('문장을 등록하였습니다!');</script>";
+  header("Refresh: 0; URL=book_detail.php?bid=$bid");
   ?>
