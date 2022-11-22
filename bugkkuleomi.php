f2baf2a4ba216b2bcbdd66e7d041fa80b24ebc6c
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-11-22 21:45:04 +0900

    Update bugkkuleomi.php

--- bugkkuleomi.php
+++ bugkkuleomi.php
@@ -4,7 +4,7 @@
 ?>
     <h2>북꾸러미</h2>
 <?php
-  $query = "SELECT PHASE.PID, PHASE.ROUTE, BOOK.NAME
+  $query = "SELECT PHASE.PID, PHASE.SENTENCE, BOOK.NAME
            FROM PHASE,BOOK
            WHERE PHASE.BID = BOOK.BID";
 
@@ -16,21 +16,11 @@ while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
     echo "저장된 문구가 없습니다.";
   }
   $pid = $row['PID'];
-  $route = $row['ROUTE'];
+  $sentence = $row['SENTENCE'];
   $name = $row['NAME'];
 ?>
   <p> <a href="phase_detail.php?pid=<?=$pid?>"> <?= $name?> </a></p>
+  <p> <?= $sentence?> </br> </br> </a></p>
   <?php
-  //파일 열기
-  $fp = fopen($route, "r") or die("문장을 불러올 수 없습니다.");
-  // 파일 내용 출력
-
-  while( !feof($fp) ) {
-    $member = fgets($fp); // 한 줄씩 $member 변수에 저장하고
-    echo $member."<br>";
-  }
-
-  // 파일 닫기
-  fclose($fp);
   }
 ?>
