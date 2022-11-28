7c9bea4d1547c6cca4278da74efff0efdeea2701
efjwbe <choeun123456@daum.net>
2022-11-28 21:33:27 +0900

    added calendar

--- attendance.php
+++ attendance.php
@@ -2,7 +2,22 @@
  require('menu.php');
 
 ?>
-    <h2>출석체크</h2>
+
+<nav class="navbar bg-light">
+  <div class="container-fluid" style="padding: 0px 75px 0px 60px;">
+    <a class="navbar-brand" style="font-size:2em;"><b>출석체크</b></a>
+      <form class="d-flex" role="search" method="POST" action="search.php">
+        <input class="form-control me-2" type="text" name="book_name" placeholder="책 제목" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+  </div>
+</nav>
+
+<form method="POST" action="attendance_process.php"><!-- 출석 버튼 -->
+  <input type="hidden" name="id" value="<?= $id ?>"/>
+  <p> <input type="submit" value="출석하기"/> </p>
+</form>
+
 <?php
   $query = "SELECT COUNTS
          FROM ATTEND
@@ -15,10 +30,27 @@
     $counts = $row['COUNTS'];
     echo "누적 출석 횟수: ".$counts;
 
-  }
+  }?>
 
-?>
-<form method="POST" action="attendance_process.php"><!-- 출석 버튼 -->
-  <input type="hidden" name="id" value="<?= $id ?>"/>
-  <p> <input type="submit" value="출석하기"/> </p>
-</form>
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <link href='.\node_modules\fullcalendar\main.css' rel='stylesheet' />
+    <script src='.\node_modules\fullcalendar\main.js'></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
+  </head>
+  <body>
+    <div id='calendar' ></div>
+  </body>
+</html>
