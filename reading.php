9590b4f4683169a7824fc95c8bd24f5d93d4c115
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2022-12-08 14:44:50 +0900

    Update reading.php

--- reading.php
+++ reading.php
@@ -1,7 +1,21 @@
 <?php
  require('menu.php');
  $bid = $_POST['bid'];
+
+$query = "SELECT NAME, ROUTE
+         FROM BOOK
+         WHERE BID = ?";
+
+ $stmt = $conn -> prepare($query);
+ $stmt -> execute(array($bid));
+
+if ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+  $name = $row['NAME'];
+  $route = $row['ROUTE'];
+}
+
 ?>
+<h2><?= $name?></h2>
 
 <form method="POST" action="book_process.php"><!-- 책갈피 -->
   <input type="hidden" name="bid" value="<?= $bid ?>"/>
@@ -17,22 +31,9 @@
 </form>
 
 <?php
-$query = "SELECT NAME, ROUTE
-         FROM BOOK
-         WHERE BID = ?";
-
- $stmt = $conn -> prepare($query);
- $stmt -> execute(array($bid));
-
-if ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-  $name = $row['NAME'];
-  $route = $row['ROUTE'];
-}
-
 //파일 열기
 $fp = fopen($route, "r") or die("책을 불러올 수 없습니다！");
 // 파일 내용 출력
-echo $name."<br><br>";
 
 while( !feof($fp) ) {
   $member = fgets($fp); // 한 줄씩 $member 변수에 저장하고
