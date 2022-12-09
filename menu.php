282a22f7831ff8b7fd05ec203bceeb9a31db0850
yujin1213 <jyoojin1597@naver.com>
2022-12-09 21:46:44 +0900

    modify menu to fixed

--- menu.php
+++ menu.php
@@ -27,7 +27,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-light fixed-top">
   <div class="container-fluid" style="padding: 0px 75px 0px 60px;">
     <a href="total_book.php">
       <img src=".\assets\BookkubBookku_logo_h.png" alt="Logo" width="265" height="100" class="d-inline-block align-text-top">
@@ -52,3 +52,5 @@
       </div>
     </div>
   </nav>
+  <body style="padding-top:105px;"\>
+
