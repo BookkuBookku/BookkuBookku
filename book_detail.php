ff36dd8546cb154dc7abfb46f14c277beea8245b
yujin1213 <jyoojin1597@naver.com>
2022-12-09 21:40:52 +0900

    add book_detail css

--- book_detail.php
+++ book_detail.php
@@ -17,17 +17,30 @@
  }
 
   ?>
-  <h2><?= $name?></h2>
-  <p>작가: <?= $author?> </p>
 
-  <form method="POST" action="reading.php"><!-- 읽기 -->
-    <input type="hidden" name="bid" value="<?= $bid ?>"/>
-    <p> <input type="submit" value="읽기"/> </p>
-  </form>
+  <link href=".\css\book_detail.css" rel="stylesheet" type="text/css" />
+  <section class="book_detail_section">
+    <div class="book_cover">
+      <p class="cover_title"><?= $name?></p>
+    </div>
+    <div class="book_info_div">
+      <div class="book_info">
+        <p class="book_name"> <?= $name?> </p>
+        <p class="book_author"> 저자명: <?= $author?> </p>
+      </div>
+      <div class="buttons">
+        <form method="POST" action="reading.php"><!-- 읽기 -->
+          <input type="hidden" name="bid" value="<?= $bid ?>"/>
+          <input type="submit" value="읽기"/>
+        </form>
 
-  <form method="POST" action="book_process.php"><!-- 찜 -->
-    <input type="hidden" name="bid" value="<?= $bid ?>"/>
-    <input type="hidden" name="id" value="<?= $id ?>"/>
-    <input type="hidden" name="status" value="like"/>
-    <p> <input type="submit" value="찜"/> </p>
-  </form>
+        <form method="POST" action="book_process.php"><!-- 찜 -->
+          <input type="hidden" name="bid" value="<?= $bid ?> "/>
+          <input type="hidden" name="id" value="<?= $id ?>"/>
+          <input type="hidden" name="status" value="like"/>
+          <p> <input type="submit" value="찜"/> </p>
+        </form>
+      </div> 
+    </div>
+  </section>
+ 
