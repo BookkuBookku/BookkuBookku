621f64a7f4d9958fb77f76edffb87027ffdabd97
yujin1213 <jyoojin1597@naver.com>
2022-12-09 21:44:30 +0900

    add bugkkuleomi css

--- bugkkuleomi.php
+++ bugkkuleomi.php
@@ -3,6 +3,10 @@
 
 ?>
 
+<link href=".\css\bookkuleomi.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css"> 
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> 
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <nav class="navbar bg-light">
   <div class="container-fluid" style="padding: 0px 75px 0px 60px;">
     <a class="navbar-brand" style="font-size:2em;">북꾸러미</a>
@@ -12,25 +16,33 @@
       </form>
   </div>
 </nav>
-
-<?php
-  $query = "SELECT PHASE.PID, PHASE.SENTENCE, BOOK.NAME
-           FROM PHASE,BOOK
-           WHERE PHASE.BID = BOOK.BID";
-
-  $stmt = $conn -> prepare($query);
-  $stmt -> execute();
-
-while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
-  if(empty($row)){
-    echo "저장된 문구가 없습니다.";
-  }
-  $pid = $row['PID'];
-  $sentence = $row['SENTENCE'];
-  $name = $row['NAME'];
-?>
-  <p> <a href="phase_detail.php?pid=<?=$pid?>"> <?= $name?> </a></p>
-  <p> <?= $sentence?> </br> </br> </a></p>
+<section class="bookku_section" style="overflow: hidden;">
   <?php
-  }
-?>
+    $query = "SELECT PHASE.PID, PHASE.SENTENCE, BOOK.NAME
+            FROM PHASE,BOOK
+            WHERE PHASE.BID = BOOK.BID";
+
+    $stmt = $conn -> prepare($query);
+    $stmt -> execute();
+
+  while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){//결과를 출력한다.
+    if(empty($row)){
+      echo "저장된 문구가 없습니다.";
+    }
+    $pid = $row['PID'];
+    $sentence = $row['SENTENCE'];
+    $name = $row['NAME'];
+  ?>
+  <div class="phase_box" data-aos="fade-up">
+  <!-- <div class="phase_box"> -->
+    <a class="book_name" href="phase_detail.php?pid=<?=$pid?>"> <?= $name?> </a>
+    <p class="phase"> <?= $sentence?></p>
+    <a class="comment" href="phase_detail.php?pid=<?=$pid?>">댓글</a>
+  </div>
+    <?php
+    }
+  ?>
+</section>
+<script> 
+  AOS.init();
+</script>
